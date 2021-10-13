68ca185b425276ca7cd686f322f0de86de65c762
Daniel <82508178+TheMaker845@users.noreply.github.com>
2021-10-13 14:11:46 +0000

    finished

---
+++ summieren.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main(){
+  int zahl_1;
+  int zahl_2;
+  ifstream fin("daten.txt");
+  ofstream fout("datensumme.txt");
+  for (int x; x<234; ++x) {
+    fin >> zahl_1;
+    fin >> zahl_2;
+    int out = zahl_1 + zahl_2;
+    cout << zahl_1 << " + " << zahl_2 << " = " << out << endl;
+    fout << out << endl;
+  }
+  fin.close();
+  fout.close();
+
+}
